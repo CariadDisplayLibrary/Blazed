0f608b41eb8c00479a3accde55e54b5d58122115
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 00:35:21 +0100

    Initial import

---
+++ Blazed.h
@@ -0,0 +1,29 @@
+#ifndef _BLAZED_H
+#define _BLAZED_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t Blazed10[];
+    extern const uint8_t Blazed11[];
+    extern const uint8_t Blazed12[];
+    extern const uint8_t Blazed13[];
+    extern const uint8_t Blazed14[];
+    extern const uint8_t Blazed15[];
+    extern const uint8_t Blazed16[];
+    extern const uint8_t Blazed17[];
+    extern const uint8_t Blazed18[];
+    extern const uint8_t Blazed19[];
+    extern const uint8_t Blazed20[];
+    extern const uint8_t Blazed21[];
+    extern const uint8_t Blazed22[];
+    extern const uint8_t Blazed23[];
+    extern const uint8_t Blazed24[];
+    extern const uint8_t Blazed25[];
+    extern const uint8_t Blazed26[];
+    extern const uint8_t Blazed27[];
+    extern const uint8_t Blazed28[];
+    extern const uint8_t Blazed29[];
+    extern const uint8_t Blazed30[];
+};
+#endif
